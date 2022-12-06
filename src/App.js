295a6dc1534ba29bddf8133ecb02aa8174089f46
Kristal Davis <kristaldavis21@gmail.com>
2022-12-06 18:02:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
-function App() {
+function App()  {
+const url = `https://api.openweathermap.org/data/2.5/weather?q=bali&appid=71edbb6e762d9d083ab62b4933f3b2ba`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className ="App">
+      <div className="container">
+        <div className="top">
+      <div className="location">
+      <p> Bali</p>
+      </div>
+      </div>
+      <div className="temp">
+        <h2>60</h2>
+        </div>
+        <div className="weather description">
+        <p>cloudy</p>
+        </div>
+        <div className="bottom">
+        <div className="feels">
+          <p> 65f</p>
+        </div>
+        <div className="hummidity">
+          <p> 40%</p>
+        </div>
+        <div className="wind">
+        <p> 21%</p>
+</div>
+</div>
+</div>
+</div>
   );
 }
 
